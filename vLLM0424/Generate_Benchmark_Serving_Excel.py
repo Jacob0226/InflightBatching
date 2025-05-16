1b233bcabaede623e31df66a24f494f8efa9eda4
Jacob <jacchang@amd.com>
2025-05-16 08:32:04 -0500

    Update excel helper script to include throughput

--- vLLM0424/Generate_Benchmark_Serving_Excel.py
+++ vLLM0424/Generate_Benchmark_Serving_Excel.py
@@ -12,7 +12,7 @@ def FillInCsvHeader(concurrency_list, folder, csv_writer):
     csv_writer.writerow([folder])
     header = ["#Concur"]
     for ilen in ILEN:
-        header.extend([f"{ilen}_TTFT(ms)", f"{ilen}_TPOT(s)", f"{ilen}_E2E(ms)"])
+        header.extend([f"{ilen}_TPUT(tok/s)", f"{ilen}_TTFT(ms)", f"{ilen}_TPOT(ms)", f"{ilen}_E2E(ms)"])
     csv_writer.writerow(header)
 
 def ProcessData(args):
@@ -30,15 +30,17 @@ def ProcessData(args):
             for ilen in ILEN:
                 benchamrk_log = os.path.join(folder, f"{ilen}_o{200}_c{concur}_p{num_prompts}.log")
                 with open(benchamrk_log, 'r') as file:
-                    ttft, tpot, e2el= 'None', 'None', 'None'
+                    tput, ttft, tpot, e2el = 'None', 'None', 'None', 'None'
                     for line in file:
-                        if "Mean TTFT (ms):" in line:
-                            ttft = float(line.split("Mean TTFT (ms):")[-1].strip())
+                        if "Output token throughput (tok/s):" in line:
+                            tput = float(line.split("Output token throughput (tok/s):")[-1].strip())
+                        elif "Mean TTFT (ms):" in line:
+                            ttft = float(line.split("Mean TTFT (ms):")[-1].strip()) 
                         elif "Mean TPOT (ms):" in line:
-                            tpot = float(line.split("Mean TPOT (ms):")[-1].strip())
+                            tpot = float(line.split("Mean TPOT (ms):")[-1].strip()) 
                         elif "Mean E2EL (ms):" in line:
-                            e2el= float(line.split("Mean E2EL (ms):")[-1].strip())
-                row_data.extend([ttft, tpot, e2el])       
+                            e2el= float(line.split("Mean E2EL (ms):")[-1].strip()) 
+                row_data.extend([tput, ttft, tpot, e2el])       
             csv_writer.writerow(row_data)
     print(f"Successfully save into {output_filename}")
 
@@ -64,6 +66,6 @@ if __name__ == "__main__":
 '''
 python Generate_Benchmark_Serving_Excel.py \
     --folder Result/0516/DiffSeed/Llama-3.1-8B/ \
-    --o Result/0516/DiffSeed/Llama-3.1-8B.csv
+    --o Result/0516/DiffSeed/SNOW_Llama-3.1-8B.csv
 
 '''
