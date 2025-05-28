713ca6589d5007d24f30e46c51f181c7d2c35cce
Jacob <jacchang@amd.com>
2025-05-28 03:29:24 -0500

    Edit vLLM script benchmark_dataset.py to generate the replicate prompts to achieve the highest prefix hit rate.

--- vLLM0424/run_benchmark_serving.sh
+++ vLLM0424/run_benchmark_serving.sh
@@ -2,8 +2,8 @@
 set -x
 
 # Sample cmd:
-#   ./run_benchmark_serving.sh --model-name meta-llama/Llama-3.1-8B
-#   ./run_benchmark_serving.sh --model-name amd/Llama-3.3-70B-Instruct-FP8-KV
+#   ./run_benchmark_serving_FixedSeed.sh --model-name meta-llama/Llama-3.1-8B  [--v0/--v1]
+#   ./run_benchmark_serving_FixedSeed.sh --model-name amd/Llama-3.3-70B-Instruct-FP8-KV [--v0/--v1]
 
 # Env var:
 export VLLM_USE_TRITON_FLASH_ATTN=0
@@ -14,10 +14,8 @@ export VLLM_FP8_WEIGHT_PADDING=1
 export VLLM_FP8_REDUCE_CONV=1
 export HIP_FORCE_DEV_KERNARG=1
 
-export VLLM_USE_V1=1
-
 # Function to parse options from any order of arguments
-# Function to parse options from any order of arguments
+Use_V1=true
 parse_args() {
     while [ $# -gt 0 ]; do
         case "$1" in
@@ -25,10 +23,21 @@ parse_args() {
                 MODEL_NAME="$2"
                 shift 2
                 ;;
+            --v1)
+                Use_V1=true
+                shift 
+                ;;
+            --v0)
+                Use_V1=false
+                shift 
+                ;;
             -h|--help)
-                echo "Usage: $0 --model-name <model_name> "
+                echo "Usage: $0 --model-name <model_name> --v0/v1"
                 echo "  e.g.  ./server.sh --model-name meta-llama/Llama-3.1-8B"
                 echo "  --model-name <model_name>  model name"
+                echo "  -h, --help                 Show this help message"
+                echo "  --v1                       Enable V1 specific settings (default: true)"
+                echo "  --v0                       Disable V1 specific settings"
                 echo "  -h, --help                 Show this help message"
                 exit 0
                 ;;
@@ -48,11 +57,17 @@ parse_args() {
 # Parse the arguments
 parse_args "$@"
 
+if $Use_V1; then # Correct shell syntax for boolean check
+    export VLLM_USE_V1=1
+else
+    NUM_SCHEDULER_STEPS=10
+    unset VLLM_USE_V1
+fi
 
 MODEL_FOLDER="${MODEL_FOLDER:-/data/huggingface/hub}"
 MODEL_PATH="${MODEL_FOLDER}/${MODEL_NAME}"
 TP=1
-BT=131072 # --max-num-batched-tokens
+BT=131072 # --max-num-batched-tokens 
 QUANT=None # --quantization is ONLINE quantization. Cannot use in ServiceNow
 
 if [[ "$MODEL_NAME" == "meta-llama/Llama-3.1-8B" ]]; then
@@ -61,13 +76,17 @@ if [[ "$MODEL_NAME" == "meta-llama/Llama-3.1-8B" ]]; then
 elif [[ "$MODEL_NAME" == "amd/Llama-3.3-70B-Instruct-FP8-KV" ]]; then
     DTYPE=float16
     # Not sure if this docker (rocm/vllm-dev:nightly_rc2.2_perf_revert_test_20250513) support fp8 in v1 engine
-    KV_TYPE=fp8 
+    KV_TYPE=auto 
 fi
 
 
 # Output 
 # meta-llama/Llama-3.1-8B -> meta-llama_Llama-3.1-8B
-result_folder="Result/0516/DiffSeed/$(basename "$MODEL_NAME")"
+if $Use_V1; then
+    result_folder="Result/0528/V1_HighHit_SingleData/$(basename "$MODEL_NAME")"
+else
+    result_folder="Result/0528/V0_HighHit_SingleData/$(basename "$MODEL_NAME")"
+fi
 mkdir -p $result_folder
 
 SERVER_PORT=8000
@@ -83,7 +102,7 @@ vllm serve $MODEL_PATH \
     --kv-cache-dtype ${KV_TYPE} \
     --swap-space 16 \
     --distributed-executor-backend mp \
-    --max-num-seqs 64 \
+    --max-num-seqs 16 \
     --max-model-len 16384 \
     --max-seq-len-to-capture 16384 \
     --max-num-batched-tokens $BT \
@@ -91,7 +110,8 @@ vllm serve $MODEL_PATH \
     --no-enable-chunked-prefill \
     --disable-log-requests \
     --uvicorn-log-level warning \
-    --port $SERVER_PORT  &
+    --port $SERVER_PORT  \
+    ${NUM_SCHEDULER_STEPS:+--num-scheduler-steps "$NUM_SCHEDULER_STEPS"} & \
     # 2>&1 | tee "${server_log}" 
     # --quantization $QUANT  # error: argument --quantization/-q: invalid choice: 'None'
 
@@ -112,15 +132,24 @@ ILEN=(2000 4000 8500)
 olen=200
 seed=0
 
-# Warmup
-python3 /app/vllm/benchmarks/benchmark_serving.py --host localhost --backend openai --port $SERVER_PORT \
-    --model $MODEL_PATH --dataset-name random --num-prompts 100 --ignore-eos \
-    --random-input-len 128 --random-output-len 128 --random-range-ratio 0 --seed $seed \
-    --max-concurrency 64 --percentile-metrics ttft,tpot,itl,e2el
+# Warmup. ilen=8500 include the longest prompts
+python3 /app/vllm/benchmarks/benchmark_serving.py \
+    --host localhost \
+    --backend openai \
+    --port "$SERVER_PORT" \
+    --model $MODEL_PATH \
+    --dataset-name random \
+    --num-prompts 10 \
+    --random-input-len 8500 \
+    --random-output-len 200 \
+    --random-range-ratio 0 \
+    --seed $seed \
+    --max-concurrency 4 \
+    --percentile-metrics ttft,tpot,itl,e2el
 
 for concurrency in "${CONCURRENCY[@]}"; do
     for ilen in "${ILEN[@]}"; do
-        num_prompts=$((concurrency * 20))
+        num_prompts=$((concurrency * 10))
         seed=$((seed + 1))
         # Define the benchmark file path
         benchmark_file="${result_folder}/i${ilen}_o${olen}_c${concurrency}_p${num_prompts}.log"
@@ -142,7 +171,6 @@ for concurrency in "${CONCURRENCY[@]}"; do
             2>&1 | tee "${benchmark_file}"
     done  
 done
-
 
 # Kill all vllm server processes
 ps -ef | grep '[p]ython' | awk '{print $2}' | xargs kill -9  # Kill server
